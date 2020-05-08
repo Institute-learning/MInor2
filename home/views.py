b2e2f8f5fd64fb10e35a6bcd6da3e3b164eac1a6
meenal1999 <38379528+meenal1999@users.noreply.github.com>
2020-05-08 05:47:57 +0530

    Home and cources pages

--- home/views.py
+++ home/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render
-from .models import Destination
+from .models import Destination,Course
+
 #from ..Quizz.models import Course 
 
 # Create your views here.
 def index(request):
-    return render(request,'index2.html')
+    dests = Course.objects.all()
+    return render(request,'index.html', {'dests': dests})
     
 # Create your views here.
 
 def acont(request):
     dests =Destination.objects.all()
 
-    return render(request,'index2.html', {'dests': dests})
+    return render(request,'index.html', {'dests': dests})
 
 def module(request):
     dests = Destination.objects.all()
 
     return render(request, 'next2.html', {'dests': dests})
+
+def content(request):
+    dests = Destination.objects.all()
+
+    return render(request, 'coursepage.html', {'dests': dests})
+
+def about(request):
+    return  render(request,'about.html');
+
+def course(request):
+    return  render(request,'course.html');
+
+def courses(request):
+
+    return  render(request,'courses.html');
+
+def contact(request):
+    return  render(request,'contact.html');
+
+
+
+
+
+
