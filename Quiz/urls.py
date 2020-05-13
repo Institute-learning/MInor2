629beea2d0df4dfe1e45481bc993abc2e9ec4c7d
meenal1999 <38379528+meenal1999@users.noreply.github.com>
2020-05-13 19:18:14 +0530

    finalize base

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [path('abc/', include('Quizz.urls')),
     path('admin/', admin.site.urls),
     path('access/', include('access.urls')),
     path('', include('home.urls')),
-    path('log/',include('Login_Auth.urls')),
+    path('', include('Login_Auth.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='Login_Auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Login_Auth/logout.html'), name='logout'),
  ]
 
 
